24c84cc25c685f82977da096d3f285b9b4a1a4fb
Sherwin Riahi <sherwinriahi@ucsb.edu>
2026-04-28 17:24:48 -0700

    sr - fixed HelpRequest repository

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
-import edu.ucsb.cs156.example.entities.UCSBDiningCommons;
+import edu.ucsb.cs156.example.entities.HelpRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
